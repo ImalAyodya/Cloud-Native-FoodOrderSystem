d47ce593118835ea4a393e2911d025339e56349d
chamithusithmaka <IT22103918@my.sliit.lk>
2025-04-02 23:55:21 +0530

    change home page , add about us and contact pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./UserPages/Home";
+import Home from "./pages/Main/Home";
+import AboutUs from "./pages/Main/AboutUs";
+import ContactUs from "./pages/Main/ContactUs";
 function App() {
  
 
@@ -10,6 +12,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
+        {/* Add more routes as needed */}
       </Routes>
     </Router>
   );
