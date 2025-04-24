1deff7f01e97cb79161c2b794a56355a50d3efff
chamithusithmaka <IT22103918@my.sliit.lk>
2025-04-24 23:43:19 +0530

    updated node module and serverjs

--- User_Management_And_Payment_Service/backend/server.js
+++ User_Management_And_Payment_Service/backend/server.js
@@ -15,12 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
 app.use("/api/auth", authRoutes); // Use auth routes
 app.use('/api/payment', paymentRoutes);
 
